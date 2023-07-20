e7a26bc624832167a6de6c267a36aaf23793b600
support <support@ainxttech.com>
2023-07-20 16:42:12 +0530

    updtation

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 
 import { useTheme } from "@mui/material/styles";
@@ -8,13 +8,13 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import { Button, Grid, TextField } from "@mui/material";
 
 const Login = () => {
   return (
     <div>
       <Navbar />
-
-      <Box sx={{ marginTop: 10 }}>
+      <Box sx={{ marginTop: 20 }}>
         <MediaControlCard />
       </Box>
     </div>
@@ -24,33 +24,103 @@ const Login = () => {
 export default Login;
 
 function MediaControlCard() {
+  const [toggle, setToggle] = useState(false);
+
   const theme = useTheme();
 
   return (
-    <Card sx={{ display: "flex", bgcolor: "red", width: "70%", m: "auto" }}>
-      
-      <CardMedia
-        component="img"
-        sx={{ width: 500 }}
-        image="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202007/defwf.png?size=690:388"
-        alt="Live from space album cover"
-      />
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
-        <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h5">
-            Live From Space
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            color="text.secondary"
-            component="div"
-          >
-            Mac Miller
-          </Typography>
-        </CardContent>
-        <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-          <IconButton aria-label="play/pause"></IconButton>
-        </Box>
+    <Card
+      sx={{
+        display: "flex",
+
+        width: "80%",
+        height: "500px",
+        m: "auto",
+      }}
+    >
+      {toggle === true ? (
+        <CardMedia
+          component="img"
+          sx={{ width: 800 }}
+          image="https://img.etimg.com/thumb/msid-78829592,width-1070,height-580,imgsize-36654,overlay-etwealthmutualfunds/photo.jpg"
+          alt="Live from space album cover"
+        />
+      ) : (
+        <CardMedia
+          component="img"
+          sx={{ width: 800 }}
+          image="https://www.claimsjournal.com/app/uploads/2022/01/bigstock-fraud.detection-scaled.jpg"
+          alt="Live from space album cover"
+        />
+      )}
+
+      {/* sx={{ display: "flex", flexDirection: "column" }} */}
+      <Box sx={{ width: "100%" }}>
+        {toggle === true ? (
+          <CardContent sx={{ flex: "1 0 auto", mt: 4 }}>
+            <Typography component="div" variant="h5" align="center">
+              Sign-Up
+            </Typography>
+
+            <Grid sx={{ mt: 2 }}>
+              <TextField label="User Name" variant="outlined" fullWidth />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <TextField label="Email" variant="outlined" fullWidth />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <TextField label="Password" variant="outlined" fullWidth />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <TextField label="Phone" variant="outlined" fullWidth />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <Button variant="contained" fullWidth>
+                Submit
+              </Button>
+            </Grid>
+            <Grid sx={{ mt: 1 }}>
+              <Typography align="center">
+                already have account Sign In
+                <Button
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setToggle(false)}
+                >
+                  👉 here
+                </Button>
+              </Typography>
+            </Grid>
+          </CardContent>
+        ) : (
+          <CardContent sx={{ flex: "1 0 auto", mt: 4 }}>
+            <Typography component="div" variant="h5" align="center">
+              Sign-In
+            </Typography>
+
+            <Grid sx={{ mt: 2 }}>
+              <TextField label="Email" variant="outlined" fullWidth />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <TextField label="Password" variant="outlined" fullWidth />
+            </Grid>
+            <Grid sx={{ mt: 2 }}>
+              <Button variant="contained" fullWidth>
+                Submit
+              </Button>
+            </Grid>
+            <Grid sx={{ mt: 1 }}>
+              <Typography align="center">
+                not have account register
+                <Button
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setToggle(true)}
+                >
+                  👉 here
+                </Button>
+              </Typography>
+            </Grid>
+          </CardContent>
+        )}
       </Box>
     </Card>
   );
